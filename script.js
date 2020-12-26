ad85d61184503eb262bca3226095d8309e78e0c4
Deepak3211 <deepakt754@gmail.com>
2020-12-26 23:22:58 +0530

    finishing the project

---
+++ script.js
@@ -0,0 +1,48 @@
+// define UI variables
+
+let result = document.querySelector('.result');
+const generatePwd = document.querySelector('.generate');
+const clipBoard = document.querySelector('#clipboard');
+
+const symbols = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+loadEventListeners();
+// load event listeners
+
+function loadEventListeners() {
+  generatePwd.addEventListener('click', randomPwd)
+clipBoard.addEventListener('click',
+  copyPwd)
+
+
+}
+
+
+function randomPwd(e) {
+e.preventDefault();
+result.innerText =
+Math.random().toString(36).slice(2) +
+Math.random().toString(36).toUpperCase().slice(2) + symbols[Math.floor(symbols.length *Math.random())]
+}
+// console.log(result);
+
+
+
+ function copyPwd (){
+	const textarea = document.createElement('textarea');
+	const password = result.innerText;
+
+	if (!password) {
+		return;
+	}
+
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard!');
+};
+
+
+
